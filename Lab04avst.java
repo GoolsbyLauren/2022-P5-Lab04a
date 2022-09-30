2f53c65394d7c7115c53d019d2a6ee9b1b663158
LaurenG <lauren.goolsby@vtsd.com>
2022-09-30 13:08:54 -0400

    09/30 - Lab04 Complete 90 Point Version

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,21 +7,28 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, Student 90 Point Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        //End of given work ->
+        double MonthRate = (annualRate/100) / 12;
+        double numMonths = numYears * 12;
+        double numerator = MonthRate * Math.pow(1 + MonthRate, numMonths);
+        double denom = Math.pow(1 + MonthRate, numMonths) - 1;
+        double MonthPay = numerator/denom * principal;
+        //End of Monthly Pay
+        double TotalPay = MonthPay * numMonths;
+        double Totalint = TotalPay - principal;
 
+        System.out.println("Principal:\t\t\t" + "$" + principal);
+        System.out.println("Annual Rate:\t\t" + annualRate + "%");
+        System.out.println("Number of Years:\t" + numYears);
+        System.out.println("Monthly Payment:\t"+ "$" + MonthPay);
+        System.out.println("Total Payments:\t\t" + "$" + TotalPay);
+        System.out.println("Total Interest:\t\t" + "$" + Totalint);
 
-
-
-
-
-
-
-
-        System.out.println();
     }
 }
 
